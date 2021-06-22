3e424e05bcd37c987828753d1f3ab9308a8be3ce
nyusuf01 <nyusuf0001@gmail.com>
2021-06-22 16:32:26 -0400

    installed jwt gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :comics
-  resources :users
+  post '/auth/login', to: 'authentication#login'
+  get '/auth/verify', to: 'authentication#verify'
+  resources :comics do 
+    resources :comments
+  end
+  resources :users, only: :create
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,29 +10,29 @@ Comment.destroy_all
 Comic.destroy_all
 User.destroy_all
 
-@user1 = User.create!(username: 'user1', email: 'user1@email.com', password: '123456')
-@user2 = User.create!(username: 'user2', email: 'user2@email.com', password: '123456')
+@user = User.create!(username: 'user', email: 'user@email.com', password: '123456')
+@another = User.create!(username: 'another', email: 'user2@email.com', password: '123456')
 
 
 puts "#{User.count} users created"
 
-@comic1 = Comic.create!(title: 'Garfield', author: 'Jim Davis', summary: 'July 08, 1980', image_url: 'https://static.wikia.nocookie.net/garfield/images/0/0d/1980-07-08.gif/', user: @user1)
+@comic1 = Comic.create!(title: 'Garfield', author: 'Jim Davis', summary: 'July 08, 1980', image_url: 'https://static.wikia.nocookie.net/garfield/images/0/0d/1980-07-08.gif/', user: @user)
 
-@comic2 = Comic.create!(title: 'Garfield', author: 'Jim Davis', summary: 'July 27, 1981', image_url: 'https://static.wikia.nocookie.net/garfield/images/6/6c/1981-07-27.gif/', user: @user1)
+@comic2 = Comic.create!(title: 'Garfield', author: 'Jim Davis', summary: 'July 27, 1981', image_url: 'https://static.wikia.nocookie.net/garfield/images/6/6c/1981-07-27.gif/', user: @user)
 
-@comic3 = Comic.create!(title: 'Cyanide & Happiness', author: 'Ron DenBleyker', summary: 'October 16, 2020', image_url: 'https://files.explosm.net/comics/Rob/halloween.png?t=295289', user: @user2)
+@comic3 = Comic.create!(title: 'Cyanide & Happiness', author: 'Ron DenBleyker', summary: 'October 16, 2020', image_url: 'https://files.explosm.net/comics/Rob/halloween.png?t=295289', user: @another)
 
-@comic4 = Comic.create!(title: 'Garfield', author: 'Jim Davis', summary: 'July 21, 2020', image_url: 'https://static.wikia.nocookie.net/garfield/images/0/08/2020-07-21.gif/', user: @user2)
+@comic4 = Comic.create!(title: 'Garfield', author: 'Jim Davis', summary: 'July 21, 2020', image_url: 'https://static.wikia.nocookie.net/garfield/images/0/08/2020-07-21.gif/', user: @another)
 
 puts "#{Comic.count} comic strips created"
 
 
-Comment.create!(content: 'I love Garfield!', user: @user2, comic: @comic1)
+Comment.create!(content: 'I love Garfield!', user: @another, comic: @comic1)
 
-Comment.create!(content: 'LOL', user: @user1, comic: @comic1)
+Comment.create!(content: 'LOL', user: @user, comic: @comic1)
 
-Comment.create!(content: 'Spooky', user: @user1, comic: @comic3)
+Comment.create!(content: 'Spooky', user: @user, comic: @comic3)
 
-Comment.create!(content: 'LOL!', user: @user2, comic: @comic3)
+Comment.create!(content: 'LOL!', user: @another, comic: @comic3)
 
 puts "#{Comment.count} comments created"
