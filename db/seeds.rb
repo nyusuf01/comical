68aa9d6697c366695c728f3a81a09a2f8c411ed2
nyusuf01 <nyusuf0001@gmail.com>
2021-06-22 15:56:44 -0400

    edited seeds.rb and tested rails db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Comment.destroy_all
+Comic.destroy_all
+User.destroy_all
+
+@user1 = User.create!(username: 'user1', email: 'user1@email.com', password: '123456')
+@user2 = User.create!(username: 'user2', email: 'user2@email.com', password: '123456')
+
+
+puts "#{User.count} users created"
+
+@comic1 = Comic.create!(title: 'Garfield', author: 'Jim Davis', summary: 'July 08, 1980', image_url: 'https://static.wikia.nocookie.net/garfield/images/0/0d/1980-07-08.gif/', user: @user1)
+
+@comic2 = Comic.create!(title: 'Garfield', author: 'Jim Davis', summary: 'July 27, 1981', image_url: 'https://static.wikia.nocookie.net/garfield/images/6/6c/1981-07-27.gif/', user: @user1)
+
+@comic3 = Comic.create!(title: 'Cyanide & Happiness', author: 'Ron DenBleyker', summary: 'October 16, 2020', image_url: 'https://files.explosm.net/comics/Rob/halloween.png?t=295289', user: @user2)
+
+@comic4 = Comic.create!(title: 'Garfield', author: 'Jim Davis', summary: 'July 21, 2020', image_url: 'https://static.wikia.nocookie.net/garfield/images/0/08/2020-07-21.gif/', user: @user2)
+
+puts "#{Comic.count} comic strips created"
+
+
+Comment.create!(content: 'I love Garfield!', user: @user2, comic: @comic1)
+
+Comment.create!(content: 'LOL', user: @user1, comic: @comic1)
+
+Comment.create!(content: 'Spooky', user: @user1, comic: @comic3)
+
+Comment.create!(content: 'LOL!', user: @user2, comic: @comic3)
+
+puts "#{Comment.count} comments created"
