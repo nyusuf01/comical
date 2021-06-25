5707b4a180cf5ef03e7fdf277486e096b59d1bc5
nyusuf01 <nyusuf0001@gmail.com>
2021-06-25 15:37:04 -0400

    edit package.json

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3000";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? /* link to your heroku app. Example:*/ "https://comical-api.herokuapp.com/"
+    : "http://localhost:3000";
 
 const api = axios.create({
   baseURL: baseUrl,
 });
-
 export default api;
